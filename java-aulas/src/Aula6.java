71b10eca11128da71561a9060536da5c3516d287
Leandro Jesus <pinheiroleandro7009@gmail.com>
2026-08-16 01:39:58 +0100

    feat: exercicio 6

---
+++ java-aulas/src/Aula6.java
@@ -0,0 +1,17 @@
+public class Aula6 {
+    public static void main(String[] args) {
+
+        String nome = "Leandro";
+        byte idade = 20;
+        double salario = 15.500;
+        char genero = 'M';
+        boolean programmer = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salario: " + salario);
+        System.out.println("Genero: " + genero);
+        System.out.println("Programador: " + programmer);
+
+    }
+}
